8df83932724c9c1a99080a59a624740d9335a05b
coding-rahul <rahul.gs9864@gmail.com>
2023-01-11 12:40:34 +0530

    Bardata page updated

--- src/components/Bardata.js
+++ src/components/Bardata.js
@@ -1,69 +1,70 @@
-import { useContext } from "react";
-import { Contextdata } from "../App";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { InplaceDisplay } from 'primereact';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import Footer from "./Footer";
 
-const Bardata = () => {
+const Bardata = ({ data, flag }) => {
 
-  const user = useContext(Contextdata);
   const detail = [
     {
-      rate: 'INR',
-      currency: user.INR,
+      currency: 'INR',
+      rate: data.INR,
       name: 'India',
 
     },
     {
-      rate: 'RUB',
-      currency: user.RUB,
+      currency: 'RUB',
+      rate: data.RUB,
       name: 'Russia',
 
     },
     {
-      rate: 'PHP',
-      currency: user.PHP,
+      currency: 'PHP',
+      rate: data.PHP,
       name: 'Philippine',
 
     },
     {
-      rate: 'TRY',
-      currency: user.TRY,
+      currency: 'TRY',
+      rate: data.TRY,
       name: 'Turkish',
 
     },
     {
-      rate: 'BRL',
-      currency: user.BRL,
+      currency: 'BRL',
+      rate: data.BRL,
       name: 'Brazil',
 
     }
   ]
+  console.log(flag)
+
   return (
     <>
-      <div className='p-col-4 mt-6 col-offset-1'>
-        <BarChart
-          width={500}
-          height={350}
-          data={detail}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          barSize={30}
-        >
-          <XAxis dataKey="rate" scale="point" padding={{ left: 50, right: 40 }} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Bar dataKey="currency" fill="#8884d8" background={{ fill: '#eee' }} />
-        </BarChart>
-        <Footer detail={detail} />
-      </div>
-
+      {
+        (flag) ?
+          <div className='p-col-4 mt-1 '>
+            <BarChart
+              width={450}
+              height={300}
+              data={detail}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+              barSize={30}
+            >
+              <XAxis dataKey="currency" scale="point" padding={{ left: 50, right: 40 }} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <CartesianGrid strokeDasharray="3 3" />
+              <Bar dataKey="rate" fill="#8884d8" background={{ fill: '#eee' }} />
+            </BarChart>
+            <Footer detail={detail} />
+          </div>
+          : ""
+      }
     </>
   )
 }
