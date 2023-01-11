190f63687ecd03f42af2ed7e9244b41c67dc93f4
coding-rahul <rahul.gs9864@gmail.com>
2023-01-11 13:02:24 +0530

    footer update yes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,22 +2,23 @@ const Footer = (prop) => {
     const { detail } = prop; // Object destructring
     return (
         <>
-            <div className='pt-4  mb-3 font-italic text-pink-400' >
+            <div className='pt-1 mb-3 font-italic text-pink-400' >
                 <h3>* Bar Chart is displaying analogy with USD and Five other country's currencies.</h3>
             </div>
             <p className='ml-8'>Stats :-</p>
 
             <table className='text-primary-300 pl-3 pt-1 ml-8'>
-                {
-                    detail.map((value, index) => (
-                      <thead key={index}> 
-                        <tr>
-                            <th className='pb-1'>{value.name} ({value.rate})</th>
-                            <td className='pl-3'>-<span className='ml-3 text-center'>{value.currency}</span></td>
-                        </tr>
-                      </thead>
-                    ))
-                }
+                <thead>
+                    {
+                        detail.map((value, index) => (
+                            <tr key={index} >
+                                <th className='pb-1'>{value.name} ({value.currency})</th>
+                                <td className='pl-3'>-<span className='ml-3 text-center'>{value.rate}</span></td>
+                            </tr>
+
+                        ))
+                    }
+                </thead>
             </table>
         </>
     )
